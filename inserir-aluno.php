530b6cb3cf7ed4c5879971cba4a92db8b90317f7
Luciano Gouveia <ljdgb2012@gmail.com>
2021-03-23 17:43:46 -0300

    atualizando

--- inserir-aluno.php
+++ inserir-aluno.php
@@ -5,11 +5,18 @@ use Alura\Pdo\Domain\Model\Student;
 require_once 'vendor/autoload.php';
 
 $databasePath = __DIR__.'/banco.sqlite';
-
 $pdo = new PDO('sqlite:'. $databasePath);
 
-$student = new Student(null,'Luciano', new \DateTimeImmutable('1989-07-13'));
+$student = new Student(null,'Bernardo', new DateTimeImmutable('1997-03-09'));
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES ('{$student->name()}','{$student->birthDate()->format('Y-m-d')}')";
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (?,?)";
+$statement = $pdo->prepare($sqlInsert);
+$statement->bindValue(1, $student->name());
+$statement->bindValue(2, $student->birthDate()->format('Y-m-d'));
 
-var_dump($pdo->exec($sqlInsert));
+
+
+if ($statement->execute()){
+    echo "Aluno inserido";
+}
+
